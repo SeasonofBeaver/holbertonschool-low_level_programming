a75f1bb98806c8f8119ca243c22e25481219d8df
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-17 10:58:06 +0200

    Project 1 Task 5 Attempt 1

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - beginning
+ *
+ * printf prints the text
+ *
+ * Return: 0 ends the program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return(0)
+}
