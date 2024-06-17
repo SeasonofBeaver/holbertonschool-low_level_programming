0c63bd5ee26646d336a59c3727197788a598bc9f
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-17 14:19:42 +0200

    Project 2 Task 0 Attempt 1 + setup

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+int main(void)
+{
+	if (n > 0)
+		printf("is positive");
+	else if (n < 0)
+		printf("is negative");
+	else
+		printf("is zero");
+	return (0);
+}
