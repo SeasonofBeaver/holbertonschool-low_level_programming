1e746736ec9d6878222a89e3c471bbc20f9bb339
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-17 14:47:20 +0200

    Project 2 Task 2 Attempt 1

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz")
+	return (0);
+}
