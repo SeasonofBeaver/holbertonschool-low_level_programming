4d009205eab9fa6efe020d51a7e960a740cdc644
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-17 10:36:56 +0200

    Project 1 Task 4 Attempt 2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/* int main is the required funtion for every c program
+ * puts outputs a specific input 
+ * and return 0 ends the program
+ *
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
