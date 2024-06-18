91c695eca3d42b79c5532989a67ae96118850444
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 10:14:06 +0200

    project 3, task 0, attempt 2

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,8 +8,17 @@
  * Return: 0 ends the program
  */
 
+#include "main.h"
+
 int main(void)
 {
-	printf("_putchar\n");
+	char text[9] = "_putchar";
+	int n = 0;
+
+	for (n = 0; n < 8; n++)
+	{
+		_putchar(text[n]);
+	}
+	_putchar('\n');
 	return (0);
 }
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+char _putchar(int a);
