5350f256fb5040ed7eb2d4d850fd80ffb95da1c6
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 14:48:12 +0200

    project 3, task 5, attempt 1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @n: is input
+ *
+ * Return: 0 ends the program
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
