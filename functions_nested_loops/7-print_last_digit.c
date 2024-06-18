1da69a19aee134ec066d48f2fb138409055b4cca
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 15:35:27 +0200

    project 3, task 7, attempt 1

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @c: is input
+ *
+ * Return: 0 ends the program
+ */
+
+int print_last_digit(int c)
+{
+	c = c % 10;
+	return (c);
+}
