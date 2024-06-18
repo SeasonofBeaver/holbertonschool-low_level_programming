9b4e2ddebacaf57d1c03df8b38dc8e058a062a74
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 14:24:11 +0200

    project 3, task 4, attempt 1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @c: is input
+ *
+ * Return: 0 ends the program
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
