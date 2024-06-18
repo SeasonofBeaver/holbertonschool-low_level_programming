9710ee208f2e3ab052a527c71f79babe4de11648
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 09:42:02 +0200

    project 2, task 9, attempt 1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0' ; n <= '8' ; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ';
+	}
+	putchar('9');
+	return (0);
+}
