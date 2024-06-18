5a8436736730bd853d6ec8387bdd45784a28e69d
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 09:47:45 +0200

    project 2, task 9, attempt 3

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,12 +12,15 @@ int main(void)
 {
 	int n;
 
-	for (n = '0' ; n <= '8' ; n++)
+	for (n = '0' ; n <= '9' ; n++)
 	{
 		putchar(n);
-		putchar(',');
-		putchar(' ');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
