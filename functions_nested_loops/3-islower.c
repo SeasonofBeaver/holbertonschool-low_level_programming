7b85c769443c914467f7908559876bfc8f25c9d0
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 14:11:58 +0200

    project 3, task 3, attempt 1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+#include "main.h"
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
