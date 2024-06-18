d9de7216b6f3b1a262f0f6497ce1333352553fa7
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 15:04:39 +0200

    project 3, task 6, attempt 1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @n: is input
+ *
+ * Return: 0 ends the program
+ */
+
+int _abs(int, n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
