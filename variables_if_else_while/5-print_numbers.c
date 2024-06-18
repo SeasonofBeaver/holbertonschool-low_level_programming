e8ee6e3223fd9208fef338a4271d8cd517e07057
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 09:14:25 +0200

    project 2, task 5, attempt 1

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+int main(void)
+{
+	int (n = 0)
+
+	while (n > 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
