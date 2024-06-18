b59d4de87c82e9e16b5cf1a29883b8520dd0e601
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-18 09:21:37 +0200

    project 2, task 6, attempt 1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
