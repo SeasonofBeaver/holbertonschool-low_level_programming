c1954c347b90def9a09f43a79650c92f7e1e49cd
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-19 11:16:16 +0200

    project 3, task 11, attempt 1

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - check the code
+ *
+ * Description: print
+ *
+ * @n: input
+ *
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+	int i;
+	int ones;
+	int tens;
+	
+	if (n < 98)
+		for (i = n ; i < 99 ; i++)
+			printf("%i, ", i);
+	else if (n > 98)
+		for (i = n ; i > 97 ; i--)
+			printf("%i, ", i);
+	else
+		printf("98");
+	printf("\n")
+}
