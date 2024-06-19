b12e45ed2fdf52bb5e90d5b029378402379307df
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-19 10:21:41 +0200

    project 3, task 8, attempt 1

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 24 ; i++)
+	{
+		for (j = 0 ; j < 60 ; j++)
+		{
+			if (i < 10)
+				_putchar('0');
+			_putchar('0' + i);
+			_putchar(':');
+			if (j < 10)
+				_putchar('0');
+			_putchar('0' + j);
+			_putchar('\n');
+		}
+	}
+}
