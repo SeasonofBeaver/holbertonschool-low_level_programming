a780b86e3406fc64aa327f8cc410e6947875ed08
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-19 10:26:06 +0200

    project 3, task 8, attempt 2

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -19,12 +19,18 @@ void jack_bauer(void)
 		for (j = 0 ; j < 60 ; j++)
 		{
 			if (i < 10)
+			{
 				_putchar('0');
-			_putchar('0' + i);
+				_putchar('0' + i);
+			}
+			else
+				_putchar(i);
 			_putchar(':');
 			if (j < 10)
+			{
 				_putchar('0');
-			_putchar('0' + j);
+				_putchar('0' + j);
+			}
 			_putchar('\n');
 		}
 	}
