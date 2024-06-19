54565e7abdcef6eb7dcee64cea80f1ea80031c60
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-19 14:54:27 +0200

    project 1, task 101, attempt 2

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fwrite(str, sizeof(char), strlen(str), stderr);
 	return (0);
 }
