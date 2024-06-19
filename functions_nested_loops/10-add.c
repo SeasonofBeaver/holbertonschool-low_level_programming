03a7e9fda7e17b01f48fffe6b5527c314a917aed
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-19 10:53:53 +0200

    project 3, task 10, attempt 1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - check the code
+ *
+ * Description: print
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
