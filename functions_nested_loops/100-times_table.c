1accf0d49b1bbdad79f0bcca48de28f94f1cd3c0
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-20 15:05:19 +0200

    project 3, task 100, attempt 3

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -2,30 +2,28 @@
 #include "main.h"
 
 /**
- * times_table - check the code
+ * print_times_table - check the code
  *
  * Description: print
+ *
+ * @n: input
  *
  * Return: Always 0.
  */
 
 void print_times_table(int n)
 {
-	int row;
-	int column;
-	int product;
-	int leftdigit;
-	int rightdigit;
+	int row, column, product, tens, ones, hundreds;
+
 	if ((n >= 0) && (n <= 15))
-	{
 		for (row = 0; row <= n; row++)
 		{
 			for (column = 0; column <= n; column++)
 			{
 				product = row * column;
-				leftdigit = product / 10;
-				rightdigit = product % 10;
-
+				hundreds = product / 100;
+				tens = ((product % 100) / 10);
+				ones = product % 10;
 				if (column == 0)
 					_putchar('0');
 				else if (product < 10)
@@ -34,18 +32,25 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar('0' + rightdigit);
+					_putchar('0' + ones);
+				}
+				else if (product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(tens + '0');
+					_putchar(ones + '0');
 				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(leftdigit + '0');
-					_putchar(rightdigit + '0');
+					_putchar(hundreds + '0');
+					_putchar(tens + '0');
+					_putchar(ones + '0');
 				}
 			}
 			_putchar('\n');
 		}
-	}
 }
