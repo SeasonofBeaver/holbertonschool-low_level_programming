d5e97e80c92cc6946c9858998f0da1fbae037ed0
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-21 10:08:34 +0200

    project 3, task 104, attempt 1

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * Return: 0 ends the program
+ */
+
+int main(void)
+{
+	int i;
+	long int fib0, fib1, fibNow;
+
+	fib0 = 0;
+	fib1 = 1;
+	for (i = 1 ; i < 98 ; i++)
+	{
+		fibNow = fib0 + fib1;
+		printf("%li, ", fibNow);
+		fib0 = fib1;
+		fib1 = fibNow;
+	}
+	fibNow = fib0 + fib1;
+	printf("%li\n", fibNow);
+	return (0);
+}
