d6ccc8e54b09f2d3a0b934d8133eebbea323e68e
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-21 11:15:22 +0200

    project 3, task 104, attempt 11

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -11,16 +11,38 @@
 int main(void)
 {
 	int i;
-	unsigned long int fib0, fib1, fibNow;
+	unsigned long int fib0, fib1, fibNow, fibFirstHalf, fibSecondHalf;
+	unsigned long int fib0first, fib0second, fib1first, fib1second;
 
 	fib0 = 0;
 	fib1 = 1;
-	for (i = 1 ; i < 98 ; i++)
+	for (i = 1 ; i < 99 ; i++)
 	{
-		fibNow = fib0 + fib1;
-		printf("%lu, ", fibNow);
-		fib0 = fib1;
-		fib1 = fibNow;
+		if ((fib1 + fib0) < 10000000000)
+		{
+			fibNow = fib0 + fib1;
+			printf("%lu", fibNow);
+			fib0 = fib1;
+			fib1 = fibNow;
+			fib0first = fib0 / 10000000000;
+			fib0second = fib0 % 10000000000;
+			fib1first = fib1 / 10000000000;
+			fib1second = fib1 % 10000000000;
+		}
+		else
+		{
+			fibFirstHalf = ((fib0first) + (fib1first));
+			fibSecondHalf = ((fib0second) + (fib1second));
+			printf("%lu%lu", fibFirstHalf, fibSecondHalf);
+			fib0first = fib1first;
+			fib1first = fibFirstHalf;
+			fib0second = fib1second;
+			fib1second = fibSecondHalf;
+		}
+		if (i < 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
 	fibNow = ((fib0 / 11) + (fib1 / 11));
 	printf("%lu", fibNow);
