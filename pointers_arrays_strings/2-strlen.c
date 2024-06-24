8afbb7086bd4fbf39894a6c3651cf8ff37dddea7
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-24 14:22:10 +0200

    project 2, task 2, attempt 1

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ *
+ * Description: print
+ *
+ * @s: input
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	return (sizeof(*s));
+}
