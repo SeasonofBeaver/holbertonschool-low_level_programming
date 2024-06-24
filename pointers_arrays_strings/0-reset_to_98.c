2da681ddde7bd0ea6605ef1aa42718046f807917
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-24 11:43:58 +0200

    project 2, task 0, attempt 1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - check the code
+ *
+ * Description: print
+ *
+ * @n: input
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
