4ba14a0e057fb6464ccc10bb915b91ec490d1922
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-24 09:29:48 +0200

    project 1, task 2, attempt 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - check the code
+ *
+ * Description: print
+ *
+ * @a: input 1
+ *
+ * @b: input 2
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
