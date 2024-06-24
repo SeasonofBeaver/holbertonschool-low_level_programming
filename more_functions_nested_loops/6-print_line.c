4ec539552fb737c974a617925476205cc2ffb991
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-24 09:53:48 +0200

    project 1, task 6, attempt 1

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *
+ * Description: print
+ *
+ * @n: input
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		i--;
+	}
+	_putchar('\n');
+}
