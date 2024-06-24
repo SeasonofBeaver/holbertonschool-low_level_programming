5255d5eb840df82f93f43ef5c9f95f52295ceee9
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-24 10:01:08 +0200

    project 1, task 7, attempt 1

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ *
+ * Description: print
+ *
+ * @n: input
+ *
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	while ((i < n) && (n > 0))
+	{
+		for (j = 0 ; j < i ; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+		i++
+	}
+	_putchar('\n');
+}
