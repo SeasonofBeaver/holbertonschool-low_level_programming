34e721d90615063895e5509ec75749270d09c292
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-24 14:57:22 +0200

    project 2, task 4, attempt 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -18,7 +18,7 @@ void print_rev(char *s)
 	length = 0;
 	for (i = 0 ; s[i] != '\0' ; i++)
 		length++;
-	for (i = length ; i >= 0 ; i--)
+	for (i = (length - 1) ; i >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
