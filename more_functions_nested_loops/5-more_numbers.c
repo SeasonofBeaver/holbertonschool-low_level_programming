178082cd55827a92f9d10196389f3e52b41aa053
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-24 09:46:16 +0200

    project 1, task 5, attempt 1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Description: print
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 15 ; i++)
+	{
+		_putchar('0' + (i % 10));
+		if (i > 9)
+			_putchar('0' + (i / 10));
+	}
+	_putchar('\n');
+}
