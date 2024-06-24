9c7edfeaba7a4e65164361890be0df982ff6d5cc
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-24 14:59:57 +0200

    project 2, task 5, attempt 1

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *
+ * Description: print
+ *
+ * @s: input
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i, length;
+	char t[];
+
+	length = 0;
+	for (i = 0 ; s[i] != '\0' ; i++)
+		length++;
+	for (i = (length - 1) ; i >= 0 ; i--)
+		t = t + s[i];
+	return (t);
+}
