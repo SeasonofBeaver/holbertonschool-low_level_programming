64c55d0974f8e6ade3cc96532cc4f68b39ff3d40
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-25 14:44:59 +0200

    project 3, task 2, attempt 4 (fixing too much output)

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -22,7 +22,7 @@ char *_strncpy(char *dest, char *src, int n)
 	length = 0;
 	for (i = 0 ; src[i] != '\0' ; i++)
 		length++;
-	for (i = 0 ; i < n ; i++)
+	for (i = 0 ; i <= n ; i++)
 		dest[i] = src[i];
 
 	return (dest);
