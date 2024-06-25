55cd0dda27495a2833cc64f36e175267e98c22bc
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-25 10:42:04 +0200

    project 2, task 10, attempt 8

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -16,6 +16,8 @@ int _atoi(char *s)
 	unsigned int number = 0;
 	int negative = 0;
 	int i;
+	int stillNumbers = 1;
+	int backToOthers = 0;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
@@ -26,8 +28,13 @@ int _atoi(char *s)
 			else
 				negative = 1;
 		}
-		else if ((s[i] >= '0') && (s[i] <= '9'))
+		else if ((s[i] >= '0') && (s[i] <= '9') && (stillNumbers == 1))
+		{
 			number = (number * 10) + s[i] - '0';
+			backToOthers = 1;
+		}
+		else if (backToOthers == 1)
+			stillNumbers = 0;
 	}
 	if (negative == 1)
 		number *= -1;
