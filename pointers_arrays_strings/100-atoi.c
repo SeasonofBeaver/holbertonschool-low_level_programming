a661804a2287045d697aa2af84386e901ac21a5d
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-25 10:32:20 +0200

    project 2, task 10, attempt 6

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,15 +14,10 @@
 int _atoi(char *s)
 {
 	int number = 0;
-	int rightHalf = 0;
 	int negative = 0;
-	int i, length;
+	int i;
 
-	length = 0;
 	for (i = 0 ; s[i] != '\0' ; i++)
-		length++;
-
-	for (i = 0 ; i < length ; i++)
 	{
 		if (s[i] == '-')
 		{
@@ -32,17 +27,10 @@ int _atoi(char *s)
 				negative = 1;
 		}
 		else if ((s[i] >= '0') && (s[i] <= '9'))
-		{
-			if (number < 100000000)
-				number = (number * 10) + s[i] - '0';
-			else
-				rightHalf = (rightHalf * 10) + s[i] - '0';
-		}
+			number = (number * 10) + s[i] - '0';
 	}
 	if (negative == 1)
 		number *= -1;
-	if (rightHalf == 0)
-		return (number);
-	else
-		return (number + rightHalf);
+
+	return (number);
 }
