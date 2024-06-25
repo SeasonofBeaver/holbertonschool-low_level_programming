2dc0abf093fc1ef8c2def8e7e20e0a161ea03e9d
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-25 14:02:13 +0200

    project 3, task 1, attempt 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - check the code
+ *
+ * Description: print
+ *
+ * @dest: input 1
+ *
+ * @src: input 2
+ *
+ * @n: input 3
+ *
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, length;
+
+	length = 0;
+	j = 0;
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		length++;
+	for (i = length ; i <= (length + n) ; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	dest[i + 1] = '\0';
+	return (dest);
+}
