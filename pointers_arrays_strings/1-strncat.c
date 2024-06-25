8c0ec9bea4a48fdc0f162764f8472625fabb1074
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-25 14:11:55 +0200

    project 3, task 1, attempt 5 (bugfixes)

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -22,7 +22,7 @@ char *_strncat(char *dest, char *src, int n)
 	lengthDest = 0;
 	lengthSrc = 0;
 	j = 0;
-	for (i = 1 ; dest[i] != '\0' ; i++)
+	for (i = 0 ; dest[i] != '\0' ; i++)
 		lengthDest++;
 	for (i = 1 ; src[i] != '\0' ; i++)
 		lengthSrc++;
