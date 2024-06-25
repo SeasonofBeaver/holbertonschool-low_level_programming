d2191af5f5aba993d65769fce6a2ba390d8fd325
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-25 14:51:28 +0200

    project 3, task 2, attempt 6 (buxfixes)

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -22,8 +22,12 @@ char *_strncpy(char *dest, char *src, int n)
 	length = 0;
 	for (i = 0 ; src[i] != '\0' ; i++)
 		length++;
-	for (i = 0 ; (i < n) && (i < length) ; i++)
-		dest[i] = src[i];
-
+	for (i = 0 ; i < n ; i++)
+	{
+		if (i < length)
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
 	return (dest);
 }
