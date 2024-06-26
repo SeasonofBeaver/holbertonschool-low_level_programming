d86179f154006f21e828dc69ae77c7eaaa4eb990
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-26 15:39:03 +0200

    project 3, task 9, attempt 4

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -13,13 +13,23 @@
 
 void print_number(int n)
 {
+	char i = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
+		if (n == INT_MIN)
+		{
+			n++;
+			i = 1;
+		}
 		n *= -1;
 	}
 
 	if (n > 9)
 		print_number(n / 10);
-	_putchar('0' + (n % 10));
+	if (i == 1)
+		_putchar('0' + n + 1);
+	else
+		_putchar('0' + (n % 10));
 }
