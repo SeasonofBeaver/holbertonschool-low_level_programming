03696af2767c615713a417ba5185997c01911bde
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-26 15:05:55 +0200

    project 3, task 8, attempt 1

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - check the code
+ *
+ * Description: print
+ *
+ * @s: input
+ *
+ * Return: Always 0.
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if ((s[i] >= 'a') || (s[i] <= 'z'))
+		{
+			s[i] = s[i] + 13;
+			while (s[i] > 122)
+				s[i] = s[i] - 26;
+		}
+	}
+}
