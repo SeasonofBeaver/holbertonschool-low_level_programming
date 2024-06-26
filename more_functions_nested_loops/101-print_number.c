be099ef7cb6b5386333b936280fb9554934860e1
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-26 11:10:19 +0200

    project 1, task 12, attempt 7 (bugfixes)

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -13,23 +13,19 @@
 
 void print_number(int n)
 {
-	int i, j, numRN, lenNum, divide, printNumber;
+	int i, j, numRN, lenNum, divide;
 
 	lenNum = 0;
 	divide = 1;
 	for (i = n ; i > 0 ; i /= 10)
 		lenNum++;
 	lenNum--;
-	if (n < 0)
-		_putchar(45);
 	for (i = n ; i > 0 ; i /= 10)
 	{
 		for (j = 0 ; j < lenNum ; j++)
 			divide = divide * 10;
 		numRN = i / divide;
 		divide = 1;
-		printNumber = numRN + 48;
-		_putchar(printNumber);
+		_putchar(numRN + '0');
 	}
-	_putchar(10);
 }
