d65589f531b330c194fe9a7bd54ca80c74b6736c
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-26 10:04:16 +0200

    project 3, task 7, attempt 1

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * leet - check the code
+ *
+ * Description: print
+ *
+ * @s: input
+ *
+ * Return: Always 0.
+ */
+
+char *leet(char *s)
+{
+	char letters[] = {a, e, o, t, l};
+	char leet[] = {'4', '3', '0', '7', '1'};
+	int i, j;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		for (j = 0 ; j < 5 ; j++)
+			if ((s[i] == letters[j]) || (s[i] == (letters[j] - 32))
+				s[i] = leet[j];
+	return (s);
+}
