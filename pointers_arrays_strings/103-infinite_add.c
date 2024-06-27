4ceeb4e921b7fb29111e31d55472e4e633f9d6c5
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-27 15:07:42 +0200

    project 3, task 11, attempt 21

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -19,7 +19,7 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, carry = 0, lenN1 = 0, lenN2 = 0, sum;
+	int i, j, temp, carry = 0, lenN1 = 0, lenN2 = 0, sum;
 
 	for (i = 0 ; n1[i] != '\0' ; i++)
 		lenN1++;
@@ -29,7 +29,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	lenN1--;
 	lenN2--;
-	for (i = size_r - 1 ; i >= 0 ; i--, lenN1--, lenN2--)
+	for (i = 0 ; i < size_r ; i++, lenN1--, lenN2--)
 	{
 		if ((lenN1 < 0) && (lenN2 < 0) && (carry == 0))
 			break;
@@ -48,6 +48,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			r[i] = ((sum + carry) % 10 + '0');
 			carry = 1;
 		}
+	}
+	for (i--, j = 0; j < i; i--, j++)
+	{
+		temp = r[i];
+		r[i] = r[j];
+		r[j] = temp;
 	}
 	return (r);
 }
