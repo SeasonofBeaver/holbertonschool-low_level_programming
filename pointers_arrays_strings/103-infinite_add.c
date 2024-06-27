5c44bbdf03647e7c303dd940a111fba3a43cd9fa
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-27 15:16:06 +0200

    project 3, task 11, attempt 24

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -25,7 +25,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		lenN1++;
 	for (i = 0 ; n2[i] != '\0' ; i++)
 		lenN2++;
-	if ((lenN1 > size_r) || (lenN2 > size_r))
+	if ((lenN1 >= size_r) || (lenN2 >= size_r))
 		return (0);
 	lenN1--;
 	lenN2--;
