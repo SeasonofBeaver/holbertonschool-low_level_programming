8d63dc0a8f3b257b748a868afccae4276e874d5b
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-28 15:13:33 +0200

    project 1, task 12, attempt idk 2

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -12,13 +12,20 @@
 
 void print_number(int n)
 {
+	int i = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
+		n++;
 		n = -n;
+		i++;
 	}
 
 	if (n > 9)
 		print_number(n / 10);
-	_putchar('0' + (n % 10));
+	if (i > 0)
+		_putchar('0' + ((n + 1) % 10));
+	else
+		_putchar('0' + (n % 10));
 }
