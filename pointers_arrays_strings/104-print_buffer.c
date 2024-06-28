4895c52970b60b8f5c3e02cd296e5753fa493f5c
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-28 10:07:35 +0200

    project 3, task 12, attempt 9

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -26,7 +26,7 @@ void print_buffer(char *b, int size)
 			{
 				if (i != 0)
 					printf("\n");
-				printf("0%02X: ", b[i]);
+				printf("0%04X: ", b[i]);
 				for (j = i ; j < i + 10 ; j++)
 				{
 					if (j >= size)
