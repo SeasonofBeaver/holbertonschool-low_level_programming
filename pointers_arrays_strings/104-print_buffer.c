94d02267363c54bcf10c676ec088e8982342fe84
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-06-28 10:19:10 +0200

    project 3, task 12, attempt 13

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -29,22 +29,15 @@ void print_buffer(char *b, int size)
 				printf("%08x: ", i);
 				for (j = i ; j < i + 10 ; j++)
 				{
-					if (j >= size)
-					{
-						if (j % 2 == 1)
-							printf("   ");
-						else
-							printf("  ");
-					}
+					if ((j >= size) && (j % 2 == 1))
+						printf("   ");
+					else if ((j >= size) && (j % 2 == 0))
+						printf("  ");
+					else if (j % 2 == 1)
+						printf("%02x ", b[j]);
 					else
-					{
-						if (j % 2 == 1)
-							printf("%02x ", b[j]);
-						else
-							printf("%02x", b[j]);
-					}
+						printf("%02x", b[j]);
 				}
-
 			}
 			if (isprint(b[i]) != 0)
 				printf("%c", b[i]);
