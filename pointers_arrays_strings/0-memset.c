dd4ecc4b1352188bad0a14b09d6ebd2375dc2633
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-01 09:30:49 +0200

    project 1, Task 0, attempt 1

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memset - check the code
+ *
+ * Description: print
+ *
+ * @s: input memory area
+ *
+ * @b: input contant byte
+ *
+ * @n: input length
+ *
+ * Return: Always 0.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		s[i] = b;
+	}
+	return (*s);
+}
