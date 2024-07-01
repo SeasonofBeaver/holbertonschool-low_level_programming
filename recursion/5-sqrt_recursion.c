f4d55531093a253d142532a7aecd34652efe96a0
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-01 15:37:06 +0200

    project 2, task 5, attempt 3

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * square_root_finder - finds if square root exists
+ *
+ * @square: input
+ *
+ * @number: input
+ *
+ * Return: root or error
+ */
+
+int square_root_finder(int square, int number)
+{
+        if (square * square == number)
+                return (square);
+        else if (square * square > number)
+        {
+                return (-1);
+        }
+        else
+                return (square_root_finder(square + 1, number));
+}
+
+/**
  * _sqrt_recursion - check the code
  *
  * Description: print
@@ -15,26 +37,4 @@ int _sqrt_recursion(int n)
 	if (n < 1)
 		return (-1);
 	return (square_root_finder(1, n));
-}
-
-/**
- * square_root_finder - finds if square root exists
- *
- * @square: input
- *
- * @number: input
- *
- * Return: root or error
- */
-
-int square_root_finder(int square, int number)
-{
-	if (square * square == number)
-		return (square);
-	else if (square * square > number)
-	{
-		return (-1);
-	}
-	else
-		return (square_root_finder(square + 1, number));
 }
