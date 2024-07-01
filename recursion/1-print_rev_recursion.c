ab421f98b8d661f89e9202cee1aeff4535fe8a6c
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-01 14:52:55 +0200

    project 2, task 1, attempt 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - check the code
+ *
+ * Description: print
+ *
+ * @s: input string
+ *
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s++);
+		_putchar(s[0]);
+	}
+	_putchar('\n');
+}
