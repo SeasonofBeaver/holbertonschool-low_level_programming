28ac3602a24f8ae98990dfafc407f89a36963620
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-01 11:01:54 +0200

    project 1, Task 5, attempt 1

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - check the code
+ *
+ * Description: print
+ *
+ * @haystack: input string to search
+ *
+ * @needle: input string to find
+ *
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, counter;
+
+	for (i = 0 ; haystack[i] != '\0' ; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			counter = i;
+			for (j = 0 ; needle[j] != '\0' ; j++)
+			{
+				if (needle[j] != haystack[counter])
+					break;
+				counter++;
+			}
+			if (needle[j] == '\0')
+				return (&haystack[i]);
+		}
+	}	
+	return (NULL);
+}
