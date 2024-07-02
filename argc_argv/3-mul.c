8a02200308d7ff1d63a12870fe20f20df7ff7862
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-02 09:48:26 +0200

    project 3, task 3, attempt 1

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @argc: number of arguments in argv
+ *
+ * @argv: array of arguments
+ *
+ * Return: 0 ends the program
+ */
+
+int main(int argc, char **argv)
+{
+	int sum;
+
+	(void) argc;
+	if ((isdigit(argv[1])) && (isdigit(argv[2])))
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
