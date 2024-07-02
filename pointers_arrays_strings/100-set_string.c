f1a0dc20c6a25701a83654664adc0e753af27c9d
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-02 10:41:54 +0200

    project 1, task 8, attempt 1

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_string - check the code
+ *
+ * Description: print
+ *
+ * @s: input string into
+ *
+ * @to: input string change
+ *
+ * Return: Always 0.
+ */
+
+void set_string(char **s, char *to)
+{
+	s = &to;
+}
