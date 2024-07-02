1c56821ef3cf5e0a4d3431839cd5918948ed552c
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-02 10:13:44 +0200

    project 3, task 4, attempt 2 (more than just 2 numbers can be added now)

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -17,22 +17,22 @@
 
 int main(int argc, char **argv)
 {
-	int sum;
+	int sum, i;
 
-	(void) argc;
 	if (argc > 2)
 	{
-		if ((atoi(argv[1]) > 0) && (atoi(argv[2]) > 0))
+		for (i = 1 ; i < argc ; i++)
 		{
-			sum = atoi(argv[1]) + atoi(argv[2]);
-			printf("%d\n", sum);
-			return (0);
+			if (atoi(argv[i]) > 0)
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		printf("%d\n", sum);
+		return (0);
 	}
 	printf("0\n");
 	return (0);
