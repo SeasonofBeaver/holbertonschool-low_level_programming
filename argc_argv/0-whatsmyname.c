2c9bd29fb98d96f04075aa5172a27299512bb9a2
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-02 09:32:11 +0200

    project 3, task 0, attempt 1

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @argc: number of arguments in argv
+ *
+ * @argv: array of arguments
+ *
+ * Return: 0 ends the program
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%s", argv[0]);
+}
