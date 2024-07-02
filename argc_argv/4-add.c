4bc8cb6d340f2db140be7fa9cc4829a6e97ab09b
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-02 10:09:09 +0200

    project 3, task 4, attempt 1

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @argc: number of arguments in argv
+ *
+ * @argv: array of arguments
+ *
+ * Return: 0 ends the program
+ */
+
+int main(int argc, char **argv)
+{
+	int sum;
+
+	(void) argc;
+	if (argc > 2)
+	{
+		if ((atoi(argv[1]) > 0) && (atoi(argv[2]) > 0))
+		{
+			sum = atoi(argv[1]) + atoi(argv[2]);
+			printf("%d\n", sum);
+			return (0);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("0\n");
+	return (0);
+}
