cf669736d7a68d28e67c5a6750c7300c7cd8825e
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-03 10:00:39 +0200

    project 2, Task 8, attempt 12

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -13,27 +13,25 @@
  *
  * @String2: string 2
  *
+ * @holdString: holds after *
+ *
  * Return: Always 0.
  */
 
-int isIdentical(char *String1, char *String2)
+int isIdentical(char *String1, char *String2, char *holdString)
 {
 	if ((String1[0] == '\0') && (String2[0] == '\0'))
 		return (1);
-	else if ((String1[0] != String2[0]) && (String2[0] != '*'))
-		return (0);
-	else if (String2[0] != '*')
-		return (isIdentical(String1 + 1, String2 + 1));
-	else if (String2[1] == String1[0])
-		return (isIdentical(String1, String2 + 1));
-	else if (String2[1] == String1[1])
-		return (isIdentical(String1 + 1, String2 + 1));
-	else if (String2[1] == '*')
-		return (isIdentical(String1, String2 + 1));
+	else if ((String1[0] == '\0') && (String2[0] == '*'))
+		return (isIdentical(String1, String2 + 1, String2 + 1));
 	else if ((String1[0] == '\0') || (String2[0] == '\0'))
 		return (0);
-	else 
-		return (isIdentical(String1 + 1, String2));
+	else if (String2[0] == '*')
+		return (isIdentical(String1, String2 + 1, String2 + 1));
+	else if (String2[0] == String1[0])
+		return (isIdentical(String1 + 1, String2 + 1, holdString));
+	else
+		return (isIdentical(String1 + 1, holdString, holdString));
 }
 
 /**
@@ -50,5 +48,12 @@ int isIdentical(char *String1, char *String2)
 
 int wildcmp(char *s1, char *s2)
 {
-	return (isIdentical(s1, s2)); 
+	if ((s1[0] == '\0') && (s2[0] == '\0'))
+		return (1);
+	else if (s1[0] == s2[0])
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (s2[0] == '*')
+		return (isIdentical(s1, s2 + 1, s2 + 1));
+	else
+		return (0);
 }
