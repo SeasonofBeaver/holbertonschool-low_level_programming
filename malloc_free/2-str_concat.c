c7cc84b459d4115d44a0801a5347fc593dfb4550
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-08 10:36:11 +0200

    project malloc, free task 2, attempt 5

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i, j, k;
+	int i, j, k, l;
 
 	for (j = 0 ; s1[j] != '\0' ; j++)
 	;
@@ -29,8 +29,8 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	for (i = 0 ; s1[i] != '\0' ; i++)
 		str[i] = s1[i];
-	for (i = j + 1 ; s2[i] != '\0' ; i++)
-		str[i] = s2[i];
+	for (i = j + 1 , l = 0 ; s2[l] != '\0' ; i++ , l++)
+		str[i] = s2[l];
 	str[i] = '\0';
 	return (str);
 	free(str);
