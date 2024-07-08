d2054fbfc729fb8088d31ac5a7d0b048ee87ff86
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-08 10:51:47 +0200

    project malloc, free task 2, attempt 9

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,24 +17,24 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i, j, k, l;
+	int i, j = 0, k = 0, l;
 
-	for (j = 0 ; s1[j] != '\0' ; j++)
-	;
-	for (k = 0 ; s2[k] != '\0' ; k++)
-	;
+	if (s1 != NULL)
+		for (j = 0 ; s1[j] != '\0' ; j++)
+		;
+	if (s2 != NULL)
+		for (k = 0 ; s2[k] != '\0' ; k++)
+		;
 	k++;
-	if (s1 == NULL)
-		j = 0;
-	if (s2 == NULL)
-		k = 0;
 	str = malloc(j + k);
 	if (str == NULL)
 		return (NULL);
-	for (i = 0 ; s1[i] != '\0' ; i++)
-		str[i] = s1[i];
-	for (i = j , l = 0 ; s2[l] != '\0' ; i++ , l++)
-		str[i] = s2[l];
+	if (s1 != NULL)
+		for (i = 0 ; s1[i] != '\0' ; i++)
+			str[i] = s1[i];
+	if (s2 != NULL)
+		for (i = j, l = 0 ; s2[l] != '\0' ; i++, l++)
+			str[i] = s2[l];
 	str[i] = '\0';
 	return (str);
 	free(str);
