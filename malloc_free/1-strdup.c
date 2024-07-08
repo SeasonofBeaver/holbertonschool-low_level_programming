f9a48cc4fcb06cb21a4c1065b5e89edbcb9b1f76
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-08 09:57:20 +0200

    project malloc, free task 1, attempt 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @str: sting
+ *
+ * Return: 0 ends the program
+ */
+
+char *_strdup(char *str)
+{
+	char s[];
+
+	if (str == NULL)
+		return (NULL);
+	s = malloc(str);
+	if (s == NULL)
+		return (NULL);
+	return (s);
+}
