2844e6a20b2172ba380f391b0ba65f7363a866bf
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-08 11:26:14 +0200

    project malloc, free task 3, attempt 4

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -20,10 +20,10 @@ int **alloc_grid(int width, int height)
 
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
-	array = malloc(height);
+	array = (int **)malloc(sizeof(int *) * height);
 	for (i = 0 ; i < height ; i++)
 	{
-		array[i] = malloc(width);
+		array[i] = (int *)malloc(sizeof(int) * width);
 		for (j = 0 ; j < width ; j++)
 			array[i][j] = 0;
 	}
