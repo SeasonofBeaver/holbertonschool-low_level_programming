10bc4d170c5f92fedbbea2d78ccff4b43d8cffb9
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-08 09:51:14 +0200

    project malloc, free task 0, attempt 5 (bugfix)

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -22,7 +22,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	str = malloc(size);
-	if (str == NULL);
+	if (str == NULL)
 		return (NULL);
 	for (i = 0 ; i < size ; i++)
 		str[i] = c;
