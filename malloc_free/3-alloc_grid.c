0e2cbe2dfde361c9ecea37d5dbede66726521423
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-08 11:08:00 +0200

    project malloc, free task 3, attempt 1

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @width: width of array
+ *
+ * @height: height of array
+ *
+ * Return: 0 ends the program
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *array;
+
+	if ((width <= 0) || (height <= 0))
+		return (NULL);
+	array[height][width] = {0};
+	return (array);
+}
