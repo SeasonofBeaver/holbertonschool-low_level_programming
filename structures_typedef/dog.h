bd0ee86587d87abbea0a1b89b9b9636380312b1b
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-09 10:21:11 +0200

    project structures_typedef, task 0, attempt 1

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+/**
+ * struct dog - doggos
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: owner of dog
+ *
+ * Description: structure of dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
