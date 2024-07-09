66385b382b2bf6e82511bee64102d1567aab1f56
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-09 09:57:46 +0200

    project more_malloc_free task 3 attempt 1

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * array_range - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @min: min number
+ *
+ * @max: max number
+ *
+ * Return: 0 ends the program
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *var;
+
+	if (min > max)
+		return (NULL);
+	var = malloc(sizeof(int) * (max - min));
+	if (var == NULL)
+		return (NULL);
+	for (i = 0 ; i < max - min ; i++)
+		var[i] = min + i;
+	return (var);
+}
