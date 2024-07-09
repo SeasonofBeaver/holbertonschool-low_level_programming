df7ed5d4874627f7983a86a1a8b3ad3558577763
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-09 09:50:49 +0200

    project more_malloc_free task 2 attempt 2 (setting memory to 0)

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -17,11 +17,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *str;
+	int i;
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
 	str = malloc(size * nmemb);
 	if (str == NULL)
 		return (NULL);
+	for (i = 0 ; i < nmemb ; i++)
+		str[i] = 0;
 	return (str);
 }
