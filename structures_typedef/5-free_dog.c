ef9a8f64a8a43db5cccf23ecf3d781b2271af323
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-10 09:55:58 +0200

    project structures_typedef, task 5, attempt 1

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @d: dog structure
+ *
+ * Return: 0 ends the program
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
