5d197b9417e970cdb94dacac63952667711c908d
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-12 17:02:43 +0200

    project more_malloc_free, task 5, attempt 2

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,17 +1,15 @@
+#include "main.h"
 #include <stddef.h>
 #include <stdlib.h>
 
-void printError(void)
+void printError(char *str)
 {
-	_putchar("E");
-	_putchar("r");
-	_putchar("r");
-	_putchar("o");
-	_putchar("r");
+	int i;
+
+	for (i = 0 ; str[i] ; i++)
+		_putchar(str[i]);
 	_putchar("\n");
 }
-
-
 
 int get_int_len (int value)
 {	
@@ -24,7 +22,6 @@ int get_int_len (int value)
 	}
 	return l;
 }
-
 
 void printSum(int num)
 {
@@ -49,6 +46,43 @@ void multiply(int num1, int num2)
 }
 
 /**
+ * _atoi - check the code
+ *
+ * Description: print
+ *
+ * @s: input
+ *
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	unsigned int number = 0;
+	int negative = 0, i, stillNumbers = 1, backToOthers = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if ((s[i] == '-') && (stillNumbers == 1))
+		{
+			if (negative == 1)
+				negative = 0;
+			else
+				negative = 1;
+		}
+		else if ((s[i] >= '0') && (s[i] <= '9') && (stillNumbers == 1))
+		{
+			number = (number * 10) + s[i] - '0';
+			backToOthers = 1;
+		}
+		else if (backToOthers == 1)
+			stillNumbers = 0;
+	}
+	if (negative == 1)
+		number *= -1;
+	return (number);
+}
+
+/**
  * main - check the code
  *
  * Description: print
@@ -62,7 +96,7 @@ void multiply(int num1, int num2)
 
 int main(int argc, char *argv[])
 {
-	int i, j, onlyNum = 1;
+	int i, j, onlyNum = 1, num1, num2;
 	
 	if (argc != 3)
 	{
@@ -82,6 +116,8 @@ int main(int argc, char *argv[])
 		printError;
 		exit(98);
 	}
-	multiply(argv[1], argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	multiply(num1, num2);
 	return (0);
 }
