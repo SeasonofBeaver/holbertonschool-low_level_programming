7dc3fff1169ea158d06919dfcd2f0353efdee26e
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-15 10:17:44 +0200

    project funtion pointers, task 3 op functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,18 @@
+#include "3-calc.h"
+
+/**
+ * op_add - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: 0 ends the program
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
