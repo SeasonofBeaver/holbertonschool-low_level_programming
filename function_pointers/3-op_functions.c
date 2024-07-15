c1ffb8d73b8c1eeb20173e861a904e74333b8051
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-15 10:20:08 +0200

    project funtion pointers, task 3 op functions complete

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -16,3 +16,71 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: 0 ends the program
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: 0 ends the program
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: 0 ends the program
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: 0 ends the program
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
