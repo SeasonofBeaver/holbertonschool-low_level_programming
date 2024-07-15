b447c8317cfd31dda234c2c9d55232244d1dfa96
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-15 10:03:28 +0200

    project funtion pointers, task 2, attempt 1

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @size: len of array
+ *
+ * @array: array of ints
+ *
+ * @cmp: pointer for funtion
+ *
+ * Return: 0 ends the program
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, check = -1;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0 ; i < size ; i++)
+	{
+		check = cmp(array[i]);
+		if (check > 0)
+			return (check);
+	}
+	return (-1);
+}
