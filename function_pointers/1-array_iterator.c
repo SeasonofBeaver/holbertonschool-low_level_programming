aefb162d96021bf78ec225a3cf57507105adb63a
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-15 09:55:03 +0200

    project funtion pointers, task 1, attempt 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @size: len of array
+ *
+ * @array: array of ints
+ *
+ * @action: pointer for funtion
+ *
+ * Return: 0 ends the program
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0 ; i < size ; i++)
+		action(array[i]);
+}
