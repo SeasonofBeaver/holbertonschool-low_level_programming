bca29b1541319b1a1f78a815e36b0bb1a337bd9d
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-17 10:10:56 +0200

    project variadic funtions task 2 attempt 1

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @separator: seperates numbers
+ *
+ * @n: numbers
+ *
+ * Return: 0 ends the program
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list pointer;
+
+	va_start(pointer, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		if (va_arg(pointer, char *) == NULL)
+			printf("(nil)");
+		else
+			printf("%s", va_arg(pointer, char *));
+		if ((i + 1 != n) && (separator != NULL))
+			printf("%s", separator);
+	}
+	va_end(pointer);
+	printf("\n");
+}
