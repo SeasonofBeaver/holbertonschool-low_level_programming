bb65d882bd65ae3822f4863717e118eded8bf55e
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-17 10:22:41 +0200

    project variadic funtions task 2 attempt 5

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(pointer, n);
 	for (i = 0 ; i < n ; i++)
-	{	
+	{
 		string = va_arg(pointer, char *);
 		if (string == NULL)
 			printf("(nil)");
