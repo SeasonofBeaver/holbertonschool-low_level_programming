fc008a532489f5be7c77136065ad5c4096d38521
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-29 11:23:04 +0200

    project singly linked lists, task 2, attempt 3

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
 /**
+ * _strlen - check the code
+ *
+ * Description: print
+ *
+ * @s: input
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int length, i;
+
+	length = 0;
+	for (i = 0 ; s[i] != '\0' ; i++)
+		length++;
+	return (length);
+}
+
+/**
  * print_list - begining
  *
  * Description: print list
@@ -12,11 +32,23 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	nodePtr temp = malloc(sizeof(list_t));
+	list_t *temp;
+	char *string;
+
+	if (!str)
+		return (NULL);
+	temp = malloc(sizeof(list_t));
 	if (!temp)
 		return (NULL);
-	temp->next = head;
-	head = temp;
-	head->str = str;
-	return (head);
+	string = strdup(str);
+	if (!string)
+	{
+		free(temp);
+		return (NULL);
+	}
+	temp->len = _strlen(string);
+	temp->str = string;
+	temp->next = *head;
+	*head = temp;
+	return (*head);
 }
