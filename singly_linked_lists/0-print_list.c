8e294f1397ec97e717cfc62f405e9929ff4dc252
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-29 10:46:20 +0200

    project singly linked lists, task 0, attempt 2

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,28 +13,14 @@
 size_t print_list(const list_t *h)
 {
 	int length = 0, i;
-	list_t curr = &h;
 
-	while (list_t.next)
+	while (h.next)
 	{
-		curr = curr->next;
-		_putchar('[');
-		_putchar(curr.length);
-		_putchar(']');
-		_putchar(' ');
-		for (i = 0 ; i < curr.length ; i++)
-		{
-			_putchar(curr.str[i]);	
-		}
-		if (curr.str == NULL)
-		{
-			_putchar('(');
-			_putchar('n');
-			_putchar('i');
-			_putchar('l');
-			_putchar(')');
-		}
-		_putchar('\n');
+		if (h.str == NULL)
+			printf("[0] (nul)\n");
+		else
+			printf("[%i] %s\n", h->length, h->str);
+		h = h.next;
 		length++;
 	}
 	return (length);
