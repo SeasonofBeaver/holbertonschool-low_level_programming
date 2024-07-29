e68922450c960868f8daef4795b4013775497e45
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-29 11:44:21 +0200

    project singly linked lists, task 3, attempt 1

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - begining
+ *
+ * Description: free list
+ *
+ * @head: list input
+ */
+
+void free_list(list_t *head)
+{
+	list_t *curr = head;
+	list_t *next;
+
+	while (curr)
+	{
+		next = curr->next;
+		free(curr->str);
+		free(curr);
+		current = next;
+	}
+	head = NULL;
+}
