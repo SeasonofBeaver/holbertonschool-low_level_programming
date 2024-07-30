6a21cb63a6854bc124877c0b59b0135bd06b3d46
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-30 11:05:40 +0200

    project Doubly linked lists, task 8, attempt 1

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - begining
+ *
+ * Description: delete element at index
+ *
+ * @head: list input
+ *
+ * @index: index of to be deleted element
+ *
+ * Return: 1 or -1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
+
+	while (i < idx)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (-1);
+		i++;
+	}
+
+	temp->prev->next = temp->next;
+	temp->next->prev = temp->prev;
+	free(temp);
+	return (1);
+}
