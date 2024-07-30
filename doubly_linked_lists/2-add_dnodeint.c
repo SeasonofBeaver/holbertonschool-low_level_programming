1c50ab138020a1ff217d2402ae7c46c97cd68c10
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-30 09:40:27 +0200

    project Doubly linked lists, task 2, attempt 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,26 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - check the code
- *
- * Description: print
- *
- * @s: input
- *
- * Return: Always 0.
- */
-
-int _strlen(char *s)
-{
-	int length, i;
-
-	length = 0;
-	for (i = 0 ; s[i] != '\0' ; i++)
-		length++;
-	return (length);
-}
-
-/**
  * add_node - begining
  *
  * Description: print list
@@ -44,6 +24,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	temp->n = number;
 	temp->prev = NULL;
 	temp->next = *head;
+	head->prev = temp;
 	*head = temp;
 	return (*head);
 }
