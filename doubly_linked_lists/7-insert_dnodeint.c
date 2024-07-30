b2564e2589508cebf097f1574676415e428d99f9
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-30 10:28:38 +0200

    project Doubly linked lists, task 7, attempt 3

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,30 +16,29 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp;
-	dlistint_t *tail = *h;
-	dlistint_t *next;
+	dlistint_t *new;
+	dlistint_t *temp = *h;
 	unsigned int i = 0;
 
-	temp = malloc(sizeof(dlistint_t));
-	if (!temp)
-		return (NULL);
-	temp->n = n;
-	if (!*h)
-	{
-		temp->prev = NULL;
-		temp->next = NULL;
-		*h = temp;
-		return (temp);
-	}
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	while (i < idx)
 	{
-		tail = tail->next;
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
 		i++;
 	}
-	temp->prev = tail;
-	next = tail->next;
-	temp->next = next;
-	tail->next = temp;
-	return (tail);
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = temp;
+	new->next = temp->next;
+	temp->next->prev = new;
+	temp->next = new;
+	return (new);
 }
