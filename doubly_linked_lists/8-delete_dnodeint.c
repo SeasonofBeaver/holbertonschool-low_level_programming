c29857f1e2a37703c50935c9eee1a888163af9d3
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-30 11:11:15 +0200

    project Doubly linked lists, task 8, attempt 4

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	else
 	{
 		temp->prev->next = temp->next;
-		if (tmp->next != NULL)
+		if (temp->next != NULL)
 			temp->next->prev = temp->prev;
 	}
 	free(temp);
