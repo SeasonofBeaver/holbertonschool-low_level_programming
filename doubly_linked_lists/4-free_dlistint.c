9a40e3e2c515104a3679709ccd23ca06946e087e
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-30 09:58:19 +0200

    project Doubly linked lists, task 4, attempt 1

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - begining
+ *
+ * Description: free list
+ *
+ * @head: list input
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *curr = head;
+	dlistint_t *next;
+
+	while (curr)
+	{
+		next = curr->next;
+		free(curr->n);
+		free(curr->prev);
+		free(curr);
+		curr = next;
+	}
+	head = NULL;
+}
