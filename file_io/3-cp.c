a7eb081d7a035cbe19363771cf4d6a983018f290
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-08-02 12:30:13 +0200

    changed permissions

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -97,14 +97,12 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to, close_to, close_from;
 	ssize_t lenr, lenw;
 	char buffer[1024];
-	mode_t file_perm;
 
 	check97(argc);
 	fd_from = open(argv[1], O_RDONLY);
-	check98((ssize_t)fd_from, argv[1], -1, -1);
-	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	check98(fd_from, argv[1], -1, -1);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	check99(fd_to, argv[2], fd_from, -1);
 	lenr = 1024;
 	while (lenr == 1024)
 	{
