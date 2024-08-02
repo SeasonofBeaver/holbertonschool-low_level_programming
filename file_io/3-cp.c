fdbf20973f3e2d395a12723db33500639731f50c
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-08-02 12:18:53 +0200

    copy file text to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -94,26 +94,26 @@ void check100(int check, int fd)
 
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, close_from, close_to;
-	ssize_t lenRd, lenWr;
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
 	char buffer[1024];
 	mode_t file_perm;
 
 	check97(argc);
 	fd_from = open(argv[1], O_RDONLY);
-	check98(fd_from, argv[1], -1, -1);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
 	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	check99(fd_to, argv[2], fd_from, -1);
-	lenRd = 1024;
-	while (lenRd == 1024)
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
 	{
-		lenRd = read(fd_from, buffer, 1024);
-		check98(lenRd, argv[1], fd_from, fd_to);
-		lenWr = write(fd_to, buffer, lenRd);
-		if (lenWr != lenRd)
-			lenWr = -1;
-		check99(lenWr, argv[2], fd_from, fd_to);
+		lenr = read(fd_from, buffer, 1024);
+		check98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check99(lenw, argv[2], fd_from, fd_to);
 	}
 	close_to = close(fd_to);
 	close_from = close(fd_from);
